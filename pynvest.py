308f0cae7aced50b5102bcd509152ea82cef1c00
Stéfano Girardelli <girardellisb@gmail.com>
2018-04-04 18:02:13 -0300

    Creating colors class to enhance the terminal experience

---
+++ pynvest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from colors import bcolors
+
+print bcolors.OKGREEN + "\n__________________________________________________\n"
+print "# # # # # # # # " + bcolors.WARNING + "WELCOME TO PYNVEST" + bcolors.OKGREEN + " # # # # # # # #" 
+print "__________________________________________________\n" + bcolors.ENDC
+
